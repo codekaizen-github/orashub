e867966a9d4adff468e0b376d983b5854e71274b
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-07-20 12:56:17 +0000

    Use environment variables

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/codekaizen-github/wordpress-plugin-registry-oras
 
 go 1.24.5
+
+require oras.land/oras-go/v2 v2.6.0
+
+require (
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 
 	"github.com/codekaizen-github/wordpress-plugin-registry-oras/internal/andrew"
+	"oras.land/oras-go/v2"
+	"oras.land/oras-go/v2/content/memory"
+	"oras.land/oras-go/v2/registry/remote"
+	"oras.land/oras-go/v2/registry/remote/auth"
+	"oras.land/oras-go/v2/registry/remote/retry"
 )
 
 func main() {
 	fmt.Println(andrew.Thing())
+
+	registry := "ghcr.io"
+	repository := "codekaizen-github/wp-github-gist-block"
+
+	src, err := remote.NewRepository(fmt.Sprintf("%s/%s", registry, repository))
+	if err != nil {
+		panic(err)
+	}
+	// Note: The below code can be omitted if authentication is not required.
+	src.Client = &auth.Client{
+		Client: retry.DefaultClient,
+		Cache:  auth.NewCache(),
+		Credential: auth.StaticCredential(registry, auth.Credential{
+			Username: os.Getenv("REGISTRY_USERNAME"),
+			Password: os.Getenv("REGISTRY_PASSWORD"),
+		}),
+	}
+
+	dst := memory.New()
+	ctx := context.Background()
+
+	tagName := "package-latest"
+	desc, err := oras.Copy(ctx, src, tagName, dst, tagName, oras.DefaultCopyOptions)
+	if err != nil {
+		panic(err) // Handle error
+	}
+	fmt.Println(desc.Digest)
 }
